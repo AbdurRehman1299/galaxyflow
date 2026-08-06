9cf3baeee4c4fd8e67ba25ed93968137112e6742
Abdur Rehman <abdurrehman76001@gmail.com>
2026-08-06 22:23:34 +0500

    component(activity): create a component to check recent activities

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import RecentActivity from './components/RecentActivity'
+
+const App = () => {
+  return (
+    <div>
+        <RecentActivity />
+    </div>
+  )
+}
+
+export default App
