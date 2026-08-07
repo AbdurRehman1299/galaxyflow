c996402f12374fefde8c23639f36710bb124406c
Abdur Rehman <abdurrehman76001@gmail.com>
2026-08-07 16:26:29 +0500

    page(dashboard): completed the dashboard with project overview and stats

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { Toaster } from "react-hot-toast";
 import { Route, Routes } from "react-router-dom";
 import Layout from "./pages/Layout";
+import Dashboard from "./pages/Dashboard";
 
 const App = () => {
   return (
     <>
       <Toaster />
       <Routes>
-        <Route path="/" element={<Layout />}></Route>
+        <Route path="/" element={<Layout />}>
+            <Route index element={<Dashboard />} />
+        </Route>
       </Routes>
     </>
   );
