cea540976f7d6d90d73d8343ea24e78f8c558ba0
Abdur Rehman <abdurrehman76001@gmail.com>
2026-08-08 23:27:35 +0500

    feat(inngest): create an inngest function to update workspace data in db

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -80,5 +80,32 @@ const syncWorkspaceCreation = inngest.createFunction(
   },
 );
 
+// Inngest function to update workspace data in database
+const syncWorkspaceUpdation = inngest.createFunction(
+  {
+    id: "update-workspace-from-clerk",
+    triggers: { event: "clerk/organization.updated" },
+  },
+  async ({ event }) => {
+    const { data } = event;
+    await prisma.workspace.update({
+      where: {
+        id: data.id,
+      },
+      data: {
+        name: data.name,
+        slug: data.slug,
+        image_url: data.image_url,
+      },
+    });
+  },
+);
+
 // Create an empty array where we'll export future Inngest functions
-export const functions = [syncUserCreation, syncUserDeletion, syncUserUpdation];
+export const functions = [
+  syncUserCreation,
+  syncUserDeletion,
+  syncUserUpdation,
+  syncWorkspaceCreation,
+  syncWorkspaceUpdation,
+];
